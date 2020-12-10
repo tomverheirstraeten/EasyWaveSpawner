7487336c4a73a43d1f728535a989d095d14d2dc3
tomverheirstraeten <tom.verheirstraeten@student.ehb.be>
2020-12-10 15:42:43 +0100

    ci(circle-ci): config fix

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,30 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser');
+const http = require('http');
+
 const port = 3000
+
+const pg = require("knex")({
+    client: 'pg',
+    version: '9.6',
+    searchPath: ['knex', 'public'],
+    connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5432/test'
+});
+
+
+const app = express();
+http.Server(app);
+
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        // to support URL-encoded bodies
+        extended: true
+    })
+);
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
