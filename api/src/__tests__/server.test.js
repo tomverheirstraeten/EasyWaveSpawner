6d4e88de41ceb2de4f5be9a48caa9d199c56e33b
tomverheirstraeten <tom.verheirstraeten@student.ehb.be>
2020-12-29 13:13:07 +0100

    feat(server): supertest added

--- api/src/__tests__/server.test.js
+++ api/src/__tests__/server.test.js
@@ -1,7 +1,35 @@
+const supertest = require("supertest");
 const app = require("../server.js")
+const request = supertest(app);
 
 
 
+describe("server test", () => {
+    test("if server exists", async (done) => {
+
+        try {
+            const response = await request.get('/')
+            expect(response.status).toBe(200, done())
+        } catch (e) {
+            if (e) {
+                console.log(e);
+            }
+        }
+    })
+})
+describe("server test", () => {
+    test("if server exists", async (done) => {
+
+        try {
+            const response = await request.get('/')
+            expect(response.status).toBe(200, done())
+        } catch (e) {
+            if (e) {
+                console.log(e);
+            }
+        }
+    })
+})
 describe("server test", () => {
     test("if server exists", async (done) => {
 
