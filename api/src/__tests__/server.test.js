609967f24bf2479c352c8d30ccd8bc56ee8e7c40
tomverheirstraeten <tom.verheirstraeten@student.ehb.be>
2021-01-02 11:35:50 +0100

    test(server.test) : test added for get endpoints

--- api/src/__tests__/server.test.js
+++ api/src/__tests__/server.test.js
@@ -2,15 +2,8 @@ const supertest = require("supertest");
 const app = require("../server.js");
 const request = supertest(app);
 
+///SERVER TEST///
 describe("server test", () => {
-
-    test("if server exists", () => {
-
-        expect(200).toBe(200);
-    })
-})
-
-describe("server test2", () => {
 
     test("if server exists", async (done) => {
 
@@ -25,19 +18,32 @@ describe("server test2", () => {
         }
     })
 })
-
-// //commit test
-describe('getWavesFromGame/:title', () => {
-    test('check if response is created', async (done) => {
+///GET ALL GAMES///
+describe('getAllGames', () => {
+    test('If i get games back', async (done) => {
         try {
-            const response = await request.get('/getWavesFromGame/game8')
-            expect(response.status).toBe(204);
+            const response = await request.get('/getAllGames')
+            expect(response.status).toBe(200);
             done()
         } catch (error) {
             console.log(error);
         }
     })
-    test("gives back empty when game doesnt exist", async () => {
+
+
+})
+///GET WAVES BY GAME///
+describe('getWavesFromGame/:title', () => {
+    test('Caps or no caps dont matter', async (done) => {
+        try {
+            const response = await request.get('/getWavesFromGame/Game2')
+            expect(response.status).toBe(200);
+            done()
+        } catch (error) {
+            console.log(error);
+        }
+    })
+    test("gives back not found when game doesnt exist", async () => {
 
         try {
             const response = await request.get('/getWavesFromGame/game88')
@@ -48,7 +54,79 @@ describe('getWavesFromGame/:title', () => {
                 console.log(e);
             }
         }
-
-
     })
+
+})
+///GET ALL WAVES///
+describe('getAllWaves', () => {
+
+    test('If I get waves back', async (done) => {
+        try {
+            const response = await request.get('/getAllWaves')
+            expect(response.status).toBe(200);
+            done()
+        } catch (error) {
+            console.log(error);
+        }
+    })
+
+
+})
+///GET ALL WAVES BY DIFFICULTY///
+describe('getWavesByDifficulty', () => {
+
+    test('If I get waves back', async (done) => {
+        try {
+            const response = await request.get('/getWavesByDifficulty/hard')
+            expect(response.status).toBe(200);
+            done()
+        } catch (error) {
+            console.log(error);
+        }
+    })
+    test('if difficulty is hard,medium,easy or extreme', async (done) => {
+        try {
+            const response = await request.get('/getWavesByDifficulty/somethingelsethanallowed')
+            expect(response).toBeFalsy();
+            done()
+        } catch (error) {
+            console.log(error);
+        }
+    })
+    test('caps or no capps dont matter', async (done) => {
+        try {
+            const response = await request.get('/getWavesByDifficulty/Hard')
+            expect(response.status).toBe(200);
+            done()
+        } catch (error) {
+            console.log(error);
+        }
+    })
+
+
+})
+///DELETE ENTIRE GAME///
+describe('deleteGame', () => {
+
+    // test('if game is deleted', async (done) => {
+    //     try {
+    //         ///delete id of game3
+    //         const response = await request.delete('/deleteGame/:id')
+    //         .then(await request.get('/getWavesFromGame/Game3'))
+    //         done()
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // })
+    // test('if waves get deleted with game', async (done) => {
+    //     try {
+    //         const response = await request.delete('/deleteGame/:id')
+    //         expect(response.status).toBe(200);
+    //         done()
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // })
+
+
 })
